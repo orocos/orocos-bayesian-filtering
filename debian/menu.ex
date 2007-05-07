de070d894b9d2beb0893b544fbca777c730b63ce
Wim Meeussen <meeussen@willowgarage.com>
2007-05-07 08:33:06 +0000

    start with clean debian dir like peter suggested

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(orocos-rtt):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="orocos-rtt" command="/usr/bin/orocos-rtt"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liborocos-rtt 1.1.0 orocos-rtt (>> 1.1.0-0), orocos-rtt (<< 1.1.0-99)
