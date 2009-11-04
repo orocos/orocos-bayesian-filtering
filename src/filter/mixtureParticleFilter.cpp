49215e0975ca6512f7148d03dfb938b1dce03d61
Klaas Gadeyne <klaas.gadeyne@gmail.com>
2009-11-04 16:02:51 +0000

    fix bug #725: malicious code in resample step of mixtureparticlefilter, thanks Jurge van Eijck

--- src/filter/mixtureParticleFilter.cpp
+++ src/filter/mixtureParticleFilter.cpp
@@ -349,7 +349,7 @@ MixtureParticleFilter<SV,MV>::DynamicResampleStep()
     // Independent resampling for different components
     for(int i = 0 ; i< dynamic_cast<Mixture<SV> *>(this->_post)->NumComponentsGet(); i++)
     {
-        result == result && this->DynamicResampleStepOne(i);
+        result = result && this->DynamicResampleStepOne(i);
     }
     return result;
 }
@@ -438,7 +438,7 @@ MixtureParticleFilter<SV,MV>::Resample()
     // Independent resampling for different components
     for(int i = 0 ; i< dynamic_cast<Mixture<SV> *>(this->_post)->NumComponentsGet(); i++)
     {
-        result == result && this->ResampleOne(i);
+        result = result && this->ResampleOne(i);
     }
     return result;
 }
