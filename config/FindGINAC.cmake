19b577af2e86676236593256bf22addb95a3f215
Tinne De Laet <tinne.delaet@mech.kuleuven.be>
2009-12-03 12:41:14 +0000

    adapt makefiles => copied the ones of 0.7 release such that boost is selected by defaults for matrix library and random number generator

--- config/FindGINAC.cmake
+++ config/FindGINAC.cmake
@@ -1,29 +1,25 @@
 # Locate GINAC install directory
 
 # This module defines
-# GINAC_INSTALL where to find include, lib, bin, etc.
-# GINAC_FOUND, is set to true
+# GINAC location and name of library
+# GINAC_FOUND, path to ginac header files
 
 OPTION( GINAC_SUPPORT "Turn me off to disable Ginac support" OFF )
-IF(NOT GINAC_INSTALL)
-  SET( GINAC_INSTALL /usr CACHE PATH "The Ginac lib installation directory.")
-ENDIF(NOT GINAC_INSTALL)
 
 IF (GINAC_SUPPORT)
 
   MESSAGE("Searching for ginac lib")
 
-  SET(GINAC_FOUND GINAC_FOUND-NOTFOUND)
-  MARK_AS_ADVANCED(GINAC_FOUND)
-  FIND_FILE(GINAC_FOUND ginac.h ${GINAC_INSTALL}/include/ginac/ )
-  IF ( GINAC_FOUND )
-    MESSAGE("-- Looking for Ginac in ${GINAC_INSTALL}/include/ginac/ - found")
-    SET( GINAC_INCLUDE "-I${GINAC_INSTALL}/include/")
-    SET( GINAC_LIBS "-L ${GINAC_INSTALL}/lib/ -lginac") 
+  FIND_LIBRARY(GINAC ginac)
+  FIND_PATH(GINAC_FOUND ginac/ginac.h )
+  IF ( GINAC AND GINAC_FOUND )
+    MESSAGE("-- Looking for Ginac - found")
+    SET( GINAC_INCLUDE "${GINAC_FOUND}")
+    SET( GINAC_LIBS "${GINAC}") 
     MESSAGE( "-- Ginac includes ${GINAC_INCLUDE}")
     MESSAGE( "-- Ginac libs     ${GINAC_LIBS}")
-  ELSE ( GINAC_FOUND )
-    MESSAGE(FATAL_ERROR "Looking for Ginac in ${GINAC_INSTALL}/include/ginac/ - not found")
-  ENDIF ( GINAC_FOUND )
+  ELSE ( GINAC AND GINAC_FOUND )
+    MESSAGE(FATAL_ERROR "Looking for Ginac - not found")
+  ENDIF ( GINAC AND GINAC_FOUND )
 
 ENDIF (GINAC_SUPPORT)
--- config/FindMATRIX.cmake
+++ config/FindMATRIX.cmake
@@ -1,7 +1,6 @@
 # Locate MATRIX LIB install directory
 
 # This module defines
-# MATRIX_INSTALL where to find include, lib, bin, etc.
 # MATRIX_FOUND, is set to true
 
 
@@ -29,68 +28,61 @@ SET(__MATRIXWRAPPER_BOOST__ OFF)
 # install path
 # ------------
 IF(NOT MATRIX_LIB)
-  SET( MATRIX_LIB lti CACHE STRING "Which matrix library to use: lti, newmat or boost")
+  SET( MATRIX_LIB boost CACHE STRING "Which matrix library to use: lti, newmat or boost")
 ENDIF(NOT MATRIX_LIB)
-IF(NOT MATRIX_INSTALL)
-  SET( MATRIX_INSTALL /usr CACHE PATH "The Matrix lib installation directory.")
-ENDIF(NOT MATRIX_INSTALL)
 MESSAGE("Searching for matrix lib ${MATRIX_LIB}")
 
 
 # find libs
 # ---------
 IF (MATRIX_LIB STREQUAL "newmat")
-  SET(NEWMAT_FOUND NEWMAT_FOUND-NOTFOUND)
-  MARK_AS_ADVANCED(NEWMAT_FOUND)
-  FIND_FILE(NEWMAT_FOUND include.h ${MATRIX_INSTALL}/include/newmat/ )
-  IF ( NEWMAT_FOUND )
-    MESSAGE("-- Looking for Newmat in ${MATRIX_INSTALL}/include/newmat/ - found")
-    SET( MATRIX_INCLUDE "${MATRIX_INSTALL}/include/")
-    SET( MATRIX_LIBS "-L${MATRIX_INSTALL}/lib -lnewmat") 
+  FIND_LIBRARY(NEWMAT newmat )
+  FIND_PATH(NEWMAT_FOUND newmat/include.h )
+  IF ( NEWMAT AND NEWMAT_FOUND )
+    MESSAGE("-- Looking for Newmat - found")
+    SET( MATRIX_INCLUDE "${NEWMAT_FOUND}")
+    SET( MATRIX_LIBS "${NEWMAT}") 
     MESSAGE( "-- Newmat includes ${MATRIX_INCLUDE}")
     MESSAGE( "-- Newmat libs     ${MATRIX_LIBS}")
     SET(__MATRIXWRAPPER_NEWMAT__ ON)	
-  ELSE ( NEWMAT_FOUND )
-    MESSAGE( FATAL_ERROR "Looking for Newmat in ${MATRIX_INSTALL}/include/newmat/ - not found")
-  ENDIF ( NEWMAT_FOUND )
+  ELSE ( NEWMAT AND NEWMAT_FOUND )
+    MESSAGE( FATAL_ERROR "Looking for Newmat - not found")
+  ENDIF ( NEWMAT AND NEWMAT_FOUND )
 ELSE (MATRIX_LIB STREQUAL "newmat")
 
 
 IF (MATRIX_LIB STREQUAL "lti")
-  SET(LTI_FOUND LTI_FOUND-NOTFOUND)
-  MARK_AS_ADVANCED(LTI_FOUND)
-  FIND_FILE(LTI_FOUND ltiMatrix.h ${MATRIX_INSTALL}/include/ltilib/ )
-  IF ( LTI_FOUND )
-    MESSAGE("-- Looking for Lti in ${MATRIX_INSTALL}/include/ltilib/ - found")
-    SET( MATRIX_INCLUDE "${MATRIX_INSTALL}/include/")
-    SET( MATRIX_LIBS "-L${MATRIX_INSTALL}/lib/ltilib -lltir") 
+  FIND_LIBRARY(LTI ltir )
+  FIND_PATH(LTI_FOUND ltilib/ltiMatrix.h )
+  IF ( LTI AND LTI_FOUND )
+    MESSAGE("-- Looking for Lti - found")
+    SET( MATRIX_INCLUDE "${LTI_FOUND}")
+    SET( MATRIX_LIBS "${LTI}") 
     MESSAGE( "-- Lti includes ${MATRIX_INCLUDE}")
     MESSAGE( "-- Lti libs     ${MATRIX_LIBS}")
     SET(__MATRIXWRAPPER_LTI__ ON)	
-  ELSE ( LTI_FOUND )
-    MESSAGE(FATAL ERROR "Looking for Lti in ${MATRIX_INSTALL}/include/ltilib/ - not found")
-  ENDIF ( LTI_FOUND )
+  ELSE ( LTI AND LTI_FOUND )
+    MESSAGE(FATAL ERROR "Looking for Lti in - not found")
+  ENDIF ( LTI AND LTI_FOUND )
 ELSE (MATRIX_LIB STREQUAL "lti")
 
 
 IF (MATRIX_LIB STREQUAL "boost")
-  SET(BOOST_FOUND BOOST_FOUND-NOTFOUND)
-  MARK_AS_ADVANCED(BOOST_FOUND)
-  FIND_FILE(BOOST_FOUND matrix.hpp ${MATRIX_INSTALL}/include/boost/numeric/ublas/ )
+  FIND_PATH(BOOST_FOUND boost/numeric/ublas/matrix.hpp )
   IF ( BOOST_FOUND )
-    MESSAGE("-- Looking for Boost in ${MATRIX_INSTALL}/include/boost/numeric/ublas/ - found")
-    SET( MATRIX_INCLUDE "${MATRIX_INSTALL}/include/")
+    MESSAGE("-- Looking for Boost - found")
+    SET( MATRIX_INCLUDE "${BOOST_FOUND}")
     SET( MATRIX_LIBS "") 
     MESSAGE( "-- Boost includes ${MATRIX_INCLUDE}")
     MESSAGE( "-- Boost libs     ${MATRIX_LIBS}")
     SET(__MATRIXWRAPPER_BOOST__ ON)	
   ELSE ( BOOST_FOUND )
-    MESSAGE(FATAL ERROR "Looking for Boost in ${MATRIX_INSTALL}/include/boost/numeric/ublas/ - not found")
+    MESSAGE(FATAL ERROR "Looking for Boost in - not found")
   ENDIF ( BOOST_FOUND )
 ELSE (MATRIX_LIB STREQUAL "boost")
 
 
-MESSAGE( FATAL_ERROR "No valid matrix lib specified. Please choose lti or newmat")
+MESSAGE( FATAL_ERROR "No valid matrix lib specified. Please choose lti, newmat or boost")
 
 ENDIF (MATRIX_LIB STREQUAL "boost")
 ENDIF (MATRIX_LIB STREQUAL "lti")
