3d0d149ce519841433308a1a515d54de9b15b6c4
Till Hofmann <thofmann@fedoraproject.org>
2018-05-29 10:44:14 +0200

    Update FSF address in license and license headers
    The FSF address has changed a while ago, the new address is taken from
    the license template at
    https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html.

--- orocos_bfl/examples/smoother/test_nonlinear_smoother.cpp
+++ orocos_bfl/examples/smoother/test_nonlinear_smoother.cpp
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
 
 /* Demonstration program for the Bayesian Filtering Library.
    Mobile robot localization with respect to wall with different possibilities for filter
